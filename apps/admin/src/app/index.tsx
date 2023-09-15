35852237d10b8613aea1a9d8f384af74c8d8ab26
Gabe Lorenzo <gtlorenzo93@gmail.com>
2023-09-14 22:46:59 -0400

    Add header and sidebar to Admin app

--- apps/admin/src/app/index.tsx
+++ apps/admin/src/app/index.tsx
@@ -1,25 +1,31 @@
 import * as React from "react";
-import { CounterButton, Link } from "ui";
 import "./styles.css";
+import SidebarWithHeader from "./navbar";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App(): JSX.Element {
   return (
-    <div className="container">
-      <h1 className="title">
-        Admin <br />
-      </h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <Link href="https://turbo.build/repo" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://vitejs.dev/" newTab>
-          Vite
-        </Link>
-      </p>
-    </div>
+    <>
+      <ChakraProvider>
+        <SidebarWithHeader />
+      </ChakraProvider>
+      {/* <div className="container">
+        <h1 className="title">
+          Admin <br />
+        </h1>
+        <CounterButton />
+        <p className="description">
+          Built With{" "}
+          <Link href="https://turbo.build/repo" newTab>
+            Turborepo
+          </Link>
+          {" & "}
+          <Link href="https://vitejs.dev/" newTab>
+            Vite
+          </Link>
+        </p>
+      </div> */}
+    </>
   );
 }
 
